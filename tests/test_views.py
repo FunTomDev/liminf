c21c518d9c2fa4b4c081570afc9b6dd81a189de3
FunTomDev <kuraskin0205@gmail.com>
2025-03-30 00:11:24 +0100

    Adding test commit

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,7 +6,7 @@ class PageAccessTest(TestCase):
 		# Define all the pages you want to test (URL names from urls.py)
 		self.pages = [
 
-			{"name": "home", "args": []}
+			{"name": "home", "args": []},
 
 		]
 

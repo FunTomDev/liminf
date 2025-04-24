e32237e3cb83989906ec6557749474c31a41758f
FunTomDev <kuraskin0205@gmail.com>
2025-04-24 16:23:35 +0200

    Adding frontend directory

---
+++ frontend/static/js/scripts.js
@@ -0,0 +1,35 @@
+function updateTilt(event) {
+        const element = event.currentTarget;
+        console.log("Updating tilt");
+        const { width, height, left, top } = element.getBoundingClientRect();
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+
+        const deltaX = event.clientX - centerX;
+        const deltaY = event.clientY - centerY;
+
+        const rotateX = (deltaY / height) * 20; // 20 degrees of tilt on the X axis
+        const rotateY = (deltaX / width) * -20; // -20 degrees of tilt on the Y axis
+
+        element.style.setProperty('--rotate-x', `${-rotateX}deg`);
+        element.style.setProperty('--rotate-y', `${-rotateY}deg`);
+		
+    }
+
+function toggleMenu() {
+	const menu_button = document.querySelector('.menu-button');
+	const menu_icon = document.querySelector('.menu-button').querySelector('.menu-icon');
+	const mobile_nav = document.querySelector('.mobile-nav');
+	const curtain = document.querySelector('.bg-curtain');
+	document.body.classList.toggle('overflow-hidden');
+	menu_icon.classList.toggle('menu-icon__open');
+	mobile_nav.classList.toggle('mobile-nav__open');
+	menu_button.classList.toggle('menu-button__open');
+	curtain.classList.toggle('bg-curtain__open');
+	menu_icon.classList.toggle('menu-icon__close');
+	mobile_nav.classList.toggle('mobile-nav__close');
+	menu_button.classList.toggle('menu-button__close');
+	curtain.classList.toggle('bg-curtain__close');
+}
+
+document.querySelector('.menu-button').addEventListener('click', toggleMenu);

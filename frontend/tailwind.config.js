89e33bd5c67f058327a9b6329d5d122ebabb043e
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2025-04-27 23:17:05 +0200

    I don't even know what I added. Probably some node modules.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -58,16 +58,6 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
-        extend: {
-            fontFamily: {
-                quicksand: ['Quicksand', 'sans-serif'],
-            },
-            textShadow: {
-                sm: '0 1px 2px var(--tw-shadow-color)',
-                DEFAULT: '0 2px 4px var(--tw-shadow-color)',
-                lg: '0 8px 16px var(--tw-shadow-color)',
-            },
-        },
     },
     plugins: [
         /**
@@ -78,15 +68,5 @@ module.exports = {
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/aspect-ratio'),
-        plugin(function ({ matchUtilities, theme }) {
-            matchUtilities(
-              {
-                'text-shadow': (value) => ({
-                  textShadow: value,
-                }),
-              },
-              { values: theme('textShadow') }
-            )
-          }),
     ],
 }

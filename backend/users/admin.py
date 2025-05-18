3ef1b1ad534ea06ae08356dd22e6b1ed69b228df
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2025-05-18 22:03:09 +0200

    Registered users in admin panel

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import User
+
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'account_type', 'bio')
+    search_fields = ('username', 'email')
+    list_filter = ('account_type',)
+    ordering = ('username',)

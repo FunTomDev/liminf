2935a62428d506aefb96f879d646c30d5a86b04f
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2025-05-20 23:10:59 +0200

    Added solution uploading form

---
+++ backend/submissions/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+
+from .models import Solution
+
+class SolutionForm(forms.ModelForm):
+    """Form for creating and updating solutions"""
+    class Meta:
+        model = Solution
+        fields = ['title', 'description', 'content', 'file']
+        labels = {
+            'title': 'Tytuł',
+            'description': 'Opis',
+            'content': 'Treść (opcjonalnie, jeśli załączono plik)',
+            'file': 'Plik (opcjonalnie, jeśli dodano treść)',
+        }
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'rows': 30, 'cols': 90}),
+            'file': forms.ClearableFileInput(attrs={'multiple': False}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        content = cleaned_data.get('content')
+        file = cleaned_data.get('file')
+
+        if not content and not file:
+            raise forms.ValidationError("Rozwiązanie musi zawierać treść albo załączony plik. To jest błąd z formsów jak coś")
+        return cleaned_data

38739853ccbea68a3758c19684cc21a611a5e501
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2025-05-20 23:10:22 +0200

    Added problems upload url and included solutions urls

--- backend/problems/urls.py
+++ backend/problems/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
 
 	path("", views.problems, name="list"),
 	path("<slug:subject_slug>/<slug:topic_slug>/<int:problem_id>/", views.details, name="details"),
+    path("<slug:subject_slug>/<slug:topic_slug>/<int:problem_id>/", include(("submissions.urls", 'submissions'), namespace='solutions')),
     path('ajax/', views.problems_ajax, name='ajax'),
+    path("add/", views.add_problem, name="add"),
 
 ]

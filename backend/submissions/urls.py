930030c2d8a254654dd723ac84563d685a2a5235
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2025-05-20 23:11:25 +0200

    Added solutions urls

--- backend/submissions/urls.py
+++ backend/submissions/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "submissions"
 urlpatterns = [
     
-	path("<slug:subject_slug>/<slug:topic_slug>/<int:problem_id>/solution/<int:solution_id>", views.solution, name="solution-details"),
+	path("solutions/<int:solution_id>", views.solution, name="details"),
+    path("solutions/add", views.add_solution, name="add"),
+    path("solutions/toggle_helpful", views.toggle_helpful_solution, name="toggle-helpful"),
+    path('solutions/ajax/<int:solution_id>/', views.ajax_solution_detail, name='ajax-solution-detail'),
 
 ]

05712d2ee45c37378dde0fa1b1696217762ecc65
Aleksandr Kuraskin <kuraskin0205@gmail.com>
2025-05-28 22:59:37 +0200

    Added whitenoise middleware

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'pages.apps.PagesConfig',
     'curriculum.apps.CurriculumConfig',
     'materials.apps.MaterialsConfig',
@@ -72,6 +71,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'

d837c6f008864fcc330facae324ec064e029ce24
aleksandrkuraskin <01199307@pw.edu.pl>
2025-06-02 19:38:59 +0200

    Updating vite src directory

---
+++ frontend/src/js/tilter.js
@@ -0,0 +1,16 @@
+function updateTilt(event) {
+        const element = event.currentTarget;
+        const { width, height, left, top } = element.getBoundingClientRect();
+        const centerX = left + width / 2;
+        const centerY = top + height / 2;
+
+        const deltaX = event.clientX - centerX;
+        const deltaY = event.clientY - centerY;
+
+        const rotateX = (deltaY / height) * 20; // 20 degrees of tilt on the X axis
+        const rotateY = (deltaX / width) * -20; // -20 degrees of tilt on the Y axis
+
+        element.style.setProperty('--rotate-x', `${-rotateX}deg`);
+        element.style.setProperty('--rotate-y', `${-rotateY}deg`);
+		
+    }
---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+import 'vite/modulepreload-polyfill';
+import './css/style.css'
+import './js/carousel-main'
+import './js/carousel-profile'
+import './js/custom-dropdown'
+import './js/filter-toggle'
+import './js/menu-toggle'
+import './js/milkdown-setup'
+import './js/tilter'
+
+
+console.log("Hello from Vite!")

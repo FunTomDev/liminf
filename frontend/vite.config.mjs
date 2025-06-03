417c52dd3f0ba1a9a1a2cf8fc5b9cae1ff8912ed
aleksandrkuraskin <01199307@pw.edu.pl>
2025-06-03 19:57:48 +0200

    Adding vite package

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -3,18 +3,29 @@ import { resolve } from 'path'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    root: '.',  // keep it relative to frontend/
+    root: '.',
+    base: '/static/',
     plugins: [
       tailwindcss(),
     ],
+    server: {
+        origin: 'http://localhost:5173',
+        cors: true,
+    },
     build: {
-        outDir: './dist/',
-        emptyOutDir: true,
-        manifest: true,
+        manifest: "manifest.json",
+        outDir: resolve("./assets"),
         rollupOptions: {
             input: {
-                main: resolve('src/main.js'),
-            },
-        },
+                main: resolve('./src/main.js'),
+                milkdown: resolve('./src/js/milkdown-setup.js'),
+                carousel_main: resolve('./src/js/carousel-main.js'),
+                carousel_profile: resolve('./src/js/carousel-profile.js'),
+                dropdown: resolve('./src/js/custom-dropdown.js'),
+                filter_toggle: resolve('./src/js/filter-toggle.js'),
+                menu_toggle: resolve('./src/js/menu-toggle.js'),
+                tilter: resolve('./src/js/tilter.js'),
+            }
+        }
     },
 })

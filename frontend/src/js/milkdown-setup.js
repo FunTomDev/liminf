2141dd2d31f85d93a16b37def9862a35fd3b30aa
aleksandrkuraskin <01199307@pw.edu.pl>
2025-06-04 15:57:21 +0200

    Making milkdown add content to hidden textarea

--- frontend/src/js/milkdown-setup.js
+++ frontend/src/js/milkdown-setup.js
@@ -2,6 +2,8 @@ import { Crepe } from "@milkdown/crepe";
 import "@milkdown/crepe/theme/common/style.css";
 import "@milkdown/crepe/theme/nord-dark.css";
 
+const contentInput = document.getElementById("id_content")
+
 const crepe = new Crepe({
   root: "#milkdown",
   features: {
@@ -15,7 +17,18 @@ const crepe = new Crepe({
   },
 });
 
-crepe.create();
+await crepe.create();
+
+// Get markdown content
+const markdown = crepe.getMarkdown();
+
+// Register event listeners
+crepe.on((listener) => {
+  listener.markdownUpdated((ctx, markdown) => {
+    contentInput.value = markdown;
+    console.log("Current input value is ", contentInput.value)
+  });
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("form .milkdown button:not([type])").forEach(btn => {

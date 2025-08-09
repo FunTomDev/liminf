fb734b939ba36ed81f4ebf49ff0374e00ca05b6a
FunTomDev <kuraskin0205@gmail.com>
2025-08-10 00:27:19 +0200

    Adding degub check

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -40,6 +40,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", '').split(',')
+print(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 
 AUTH_USER_MODEL = 'users.User'
 
